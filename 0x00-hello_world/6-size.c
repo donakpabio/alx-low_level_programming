62d7d5252ca0a050d080d557b4cf92b92c3ecb59
Namkere Akpabio <donakpabio@gmail.com>
2022-10-13 13:41:16 -0700

    first commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,11 +16,11 @@ int main(void)
 	int d = sizeof(long long int);
 	int e = sizeof(float);
 
-	printf("Size of a char: %d byte(s)\n ", a);
-	printf("Size of a int: %d byte(s)\n ", b);
-	printf("Size of a long int: %d byte(s)\n ", c);
-	printf("Size of a long long int: %d byte(s)\n ", d);
-	printf("Size of a float: %d byte(s)\n ", e);
+	printf("Size of a char: %d byte(s)\n", a);
+	printf("Size of a int: %d byte(s)\n", b);
+	printf("Size of a long int: %d byte(s)\n", c);
+	printf("Size of a long long int: %d byte(s)\n", d);
+	printf("Size of a float: %d byte(s)\n", e);
 	/* test */
 	return (0);
 }
