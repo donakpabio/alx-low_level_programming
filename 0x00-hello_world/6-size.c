874b022419c076600392945e28d87e462956ff4a
Namkere Akpabio <donakpabio@gmail.com>
2022-10-13 13:38:03 -0700

    first commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * This is the preferred style for multi-line
  * comments in C source code.
@@ -7,18 +8,19 @@
  * Description:  A column of asterisks on the left side,
 * with beginning and ending almost-blank lines.
 */
-
 int main(void)
 {
 	int a = sizeof(char);
 	int b = sizeof(int);
-	int c = sizeof(long);
-	int d = sizeof(float);
-	int e = sizeof(short);
+	int c = sizeof(long int);
+	int d = sizeof(long long int);
+	int e = sizeof(float);
+
 	printf("Size of a char: %d byte(s)\n ", a);
 	printf("Size of a int: %d byte(s)\n ", b);
-	printf("Size of a long: %d byte(s)\n ", c);
-	printf("Size of a float: %d byte(s)\n ", d);
-	printf("Size of a short: %d byte(s)\n ", e);
+	printf("Size of a long int: %d byte(s)\n ", c);
+	printf("Size of a long long int: %d byte(s)\n ", d);
+	printf("Size of a float: %d byte(s)\n ", e);
+	/* test */
 	return (0);
 }
