9cc143badd8fb9dfb24cc2b79f463aa53f05bba3
Namkere Akpabio <donakpabio@gmail.com>
2022-10-14 01:53:42 -0700

    When I was having the alphabet soup...

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,9 +4,19 @@
  * main: Method doing everything
  * This method is used to do everything int he main method.
  * Return: Exit properly
- * /
+ */
 int main(void)
 {
+	char a;
+	for (a='a'; a<='z'; a++)
+	{
+		putchar(a);
+	}
 
+	for (a='A'; a<='Z'; a++)
+        {
+                putchar(a);
+        }
+	putchar('\n');
 	return (0);
 }
--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,9 +4,17 @@
  * main: Method doing everything
  * This method is used to do everything int he main method.
  * Return: Exit properly
- * /
+ */
 int main(void)
 {
-
+	char a;
+	for (a='a'; a<='z'; a++)
+	{
+		if(!(a=='q' || a=='e'))
+		{
+			putchar(a);
+		}
+	}
+	putchar('\n');
 	return (0);
 }
