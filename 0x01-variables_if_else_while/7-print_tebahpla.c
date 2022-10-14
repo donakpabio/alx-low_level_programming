7824db1c8fa6a19a55222f0c58618f04608805f4
Namkere Akpabio <donakpabio@gmail.com>
2022-10-14 02:12:41 -0700

    Smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -4,9 +4,14 @@
  * main: Method doing everything
  * This method is used to do everything int he main method.
  * Return: Exit properly
- * /
+ */
 int main(void)
 {
-
+	char a;
+	for (a='z'; a>='a'; a--)
+	{
+		putchar(a);
+	}
+	putchar('\n');
 	return (0);
 }
