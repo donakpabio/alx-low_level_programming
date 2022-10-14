4d0cf5b8b62b951460796d5f6aeaa06b6ba5c251
Namkere Akpabio <donakpabio@gmail.com>
2022-10-14 04:20:58 -0700

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 
 /**
- * main: Method doing everything
- * This method is used to do everything int he main method.
- * Return: Exit properly
+ * main - Entry point
+ * Description: This is a longer description.
+ * Don't forget that a line should not exceed 80 characters.
+ * But you're totally free to use several lines to properly
+ * describe your function
+ * Return: Just returning zero
  */
 int main(void)
 {
 	int a;
-	for (a=0; a<=9; a++)
+
+	for (a = 0; a <= 9; a++)
 	{
-		printf("%d",a);
+		printf("%d", a);
 	}
 	putchar('\n');
 	return (0);
