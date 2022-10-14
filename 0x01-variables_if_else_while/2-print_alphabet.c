744bd5e7dd9ece6e331ea165ceb89f2e17a63762
Namkere Akpabio <donakpabio@gmail.com>
2022-10-14 01:41:44 -0700

    I sometimes suffer from insomnia...

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,9 +4,14 @@
  * main: Method doing everything
  * This method is used to do everything int he main method.
  * Return: Exit properly
- * /
+ */
 int main(void)
 {
-
+	char a;
+	for (a='a'; a<='z'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
 	return (0);
 }
