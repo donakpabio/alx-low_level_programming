3cc50d4faec17b4813de4f7c66b59a0c2c0fe838
Namkere Akpabio <donakpabio@gmail.com>
2022-10-14 06:00:03 -0700

    Multiple files

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: This is a longer description.
+ * Don't forget that a line should not exceed 80 characters.
+ * But you're totally free to use several lines to properly
+ * describe your function
+ * Return: Just returning zero
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("%d", n);
+
+	if (n > 0)
+	{
+		printf(" is positive\n");
+	} else if ( n == 0)
+	{
+		printf(" is zero\n");
+	} else if (n < 0)
+	{
+		printf(" is negative\n");
+	}
+	
+	return (0);
+}
