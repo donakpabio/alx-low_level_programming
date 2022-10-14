3ee7e6f3b588a8e99a14c17958fd0239e4cc6c4e
Namkere Akpabio <donakpabio@gmail.com>
2022-10-14 06:02:59 -0700

    Multiple files

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -21,13 +21,12 @@ int main(void)
 	if (n > 0)
 	{
 		printf(" is positive\n");
-	} else if ( n == 0)
+	} else if (n == 0)
 	{
 		printf(" is zero\n");
 	} else if (n < 0)
 	{
 		printf(" is negative\n");
-	}
-	
+	}	
 	return (0);
 }
