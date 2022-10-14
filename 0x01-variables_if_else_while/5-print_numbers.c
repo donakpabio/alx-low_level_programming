6096dd4299d76eb95329d09f1644d35a649b9467
Namkere Akpabio <donakpabio@gmail.com>
2022-10-14 01:58:15 -0700

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,9 +4,14 @@
  * main: Method doing everything
  * This method is used to do everything int he main method.
  * Return: Exit properly
- * /
+ */
 int main(void)
 {
-
+	int a;
+	for (a=0; a<=9; a++)
+	{
+		printf("%d",a);
+	}
+	putchar('\n');
 	return (0);
 }
