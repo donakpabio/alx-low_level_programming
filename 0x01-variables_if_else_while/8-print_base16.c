38c01f487030282913003fc0d096fcbdc0ab4823
Namkere Akpabio <donakpabio@gmail.com>
2022-10-14 02:53:50 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -4,9 +4,19 @@
  * main: Method doing everything
  * This method is used to do everything int he main method.
  * Return: Exit properly
- * /
+ */
 int main(void)
 {
+	char a;
+	for (a='0'; a<='9'; a++)
+	{
+		putchar(a);
+	}
 
+	for (a='a'; a<='f'; a++)
+        {
+                putchar(a);
+        }
+	putchar('\n');
 	return (0);
 }
