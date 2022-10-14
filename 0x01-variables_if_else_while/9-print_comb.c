fba9bd4ed59d561f6fff6571962e86f6ec191c21
Namkere Akpabio <donakpabio@gmail.com>
2022-10-14 02:41:52 -0700

    Patience, persistene and perspiration

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,9 +4,19 @@
  * main: Method doing everything
  * This method is used to do everything int he main method.
  * Return: Exit properly
- * /
+ */
 int main(void)
 {
-
+	int a;
+	for (a=1; a<=9; a++)
+	{
+		putchar(a+'0');
+		if(a != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
 	return (0);
 }
