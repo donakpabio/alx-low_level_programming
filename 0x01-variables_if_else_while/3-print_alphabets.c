c5d81d5956954527d671491210389c3504923867
Namkere Akpabio <donakpabio@gmail.com>
2022-10-14 04:25:21 -0700

    Multiple files

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
 
 /**
- * main: Method doing everything
- * This method is used to do everything int he main method.
- * Return: Exit properly
+ * main - Entry point
+ * Description: This is a longer description.
+ * Don't forget that a line should not exceed 80 characters.
+ * But you're totally free to use several lines to properly
+ * describe your function
+ * Return: Just returning zero
  */
 int main(void)
 {
 	char a;
-	for (a='a'; a<='z'; a++)
+
+	for (a = 'a'; a <= 'z'; a++)
 	{
 		putchar(a);
 	}
 
-	for (a='A'; a<='Z'; a++)
-        {
-                putchar(a);
-        }
+	for (a = 'A'; a <= 'Z'; a++)
+	{
+		putchar(a);
+	}
 	putchar('\n');
 	return (0);
 }
--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 
 /**
- * main: Method doing everything
- * This method is used to do everything int he main method.
- * Return: Exit properly
+ * main - Entry point
+ * Description: This is a longer description.
+ * Don't forget that a line should not exceed 80 characters.
+ * But you're totally free to use several lines to properly
+ * describe your function
+ * Return: Just returning zero
  */
 int main(void)
 {
 	char a;
-	for (a='a'; a<='z'; a++)
+
+	for (a = 'a'; a <= 'z'; a++)
 	{
-		if(!(a=='q' || a=='e'))
+		if (!(a == 'q' || a == 'e'))
 		{
 			putchar(a);
 		}
