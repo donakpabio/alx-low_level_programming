678676a6b5acccfc485d0e9970fdb46137d4b41d
Namkere Akpabio <donakpabio@gmail.com>
2022-10-18 10:19:04 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,17 +9,20 @@
  */
 int print_sign(int n)
 {
+	int a;
+	
 	if (n > 0)
 	{
 		_putchar('+');
-		return (1);
+		a = 1);
 	} else if (n == 0)
 	{
 		_putchar('0');
-		return (0);
+		a = 0;
 	} else
 	{
 		_putchar('-');
-		return (-1);
+		a = -1;
 	}
+	return (a);
 }
