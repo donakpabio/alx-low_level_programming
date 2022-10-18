b64a2a161b1560241fc3335c1d3e26550de734b5
Namkere Akpabio <donakpabio@gmail.com>
2022-10-18 09:17:57 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,7 +5,7 @@
  * print_alphabet_x10 - check the code.
  *
  * Return: Always Nothing
- *
+ */
 void print_alphabet_x10()
 {
 	for (int a=0; a<10; a++)
@@ -18,28 +18,4 @@ void print_alphabet_x10()
 		}
 		_putchar('\n');
 	}
-}*/
-
-/**
- * _putchar - check the code.
- *
- * Return: Always Nothing
- *
-void _putchar(int c)
-{
-	putchar(c);
-}*/
-
-/**
- * main - Entry point
- * Description: This is a longer description.
- * Don't forget that a line should not exceed 80 characters.
- * But you're totally free to use several lines to properly
- * describe your function
- * Return: Just returning zero
- */
-int main(void)
-{
-	print_alphabet_x10();
-	return (0);
 }
