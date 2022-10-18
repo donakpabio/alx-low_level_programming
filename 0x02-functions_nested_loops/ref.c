c2a67709baf06a2594125dc22cefe7a4ff3a5bce
Namkere Akpabio <donakpabio@gmail.com>
2022-10-18 11:55:42 -0700

    There are only 3 colors...

--- 0x02-functions_nested_loops/ref.c
+++ 0x02-functions_nested_loops/ref.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <stdlib.h>
+#include <limits.h>
 #include "main.h"
 
 int _isalpha(int c)
@@ -8,6 +10,14 @@ int _isalpha(int c)
 	int l = islower(c) > 0 ? 1 : 0;
 	int u = isupper(c) > 0 ? 1 : 0;
 	return (a && (l || u));
+}
+
+int print_last_digit(int n)
+{
+        int r = (n < 0 ? ((unsigned int)r * -1) % 10 : ((unsigned int)r % 10));
+
+        _putchar(r + '0');
+        return (r);
 }
 
 /**
@@ -27,16 +37,22 @@ void _putchar(int c)
  */
 int main(void)
 {
-	int r;
+	int r = abs(INT_MIN);
 
-	r = _isalpha('H');
+	r = r < 0 ? (r * -1) : r;
+
+	printf("This is it %d\n", r);
+	r = print_last_digit(r);
+	_putchar('0' + r);
+	
+	/**r = _isalpha('H');
 	_putchar(r + '0');
 	r = _isalpha('o');
 	_putchar(r + '0');
 	r = _isalpha(108);
 	_putchar(r + '0');
 	r = _isalpha(';');
-	_putchar(r + '0');
+	_putchar(r + '0');*/
 	_putchar('\n');
 	return (0);
 }
