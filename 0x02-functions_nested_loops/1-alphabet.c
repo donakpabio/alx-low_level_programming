1c7ac606eea0f99ccbff011201710498879eaaf0
Namkere Akpabio <donakpabio@gmail.com>
2022-10-18 04:55:07 -0700

    Files

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,5 @@
 #include <stdio.h>
-
-
-/**
- * print_alphabet - check the code.
- *
- * Return: Always Nothing
- */
-void print_alphabet()
-{
-	char a;
-
-	for (a = 'a'; a <= 'z'; a++)
-	{
-		_putchar(a);
-	}
-	_putchar('\n');
-}
+#include "main.h"
 
 /**
  * main - check the code.
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -15,4 +15,13 @@ void _putchar(int c)
  *
  * Return: Always Nothing.
  */
-void print_alphabet();
+void print_alphabet()
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+        }
+	_putchar('\n');
+}
