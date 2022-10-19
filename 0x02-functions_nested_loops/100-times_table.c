80c01b2acd8879a4322d754c00900cb9f04420a3
Namkere Akpabio <donakpabio@gmail.com>
2022-10-18 17:13:20 -0700

    The world looks like ...

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_digit - writes the character c to stdout
+ * @d: The character to print
+ *
+ * Return: Nothing
+ */
+void print_digit(int d)
+{
+	int r = d % 10;
+
+	if ((d - r) == 0)
+	{
+		_putchar(r + '0');
+	} else
+	{
+		print_digit((d - r) / 10);
+		_putchar(r + '0');
+	}
+}
+
+/**
+ * times_table - writes the character c to stdout
+ * @n: times table
+ *
+ * Return: Nothing
+ */
+void times_table(int n)
+{
+	if (n > 0 && n <= 15)
+	{
+		int a = 0;
+
+		while (a < n)
+		{
+			int b = 0;
+
+			while (b < n)
+			{
+				int r = a * b;
+
+				if (b > 0)
+				{
+					_putchar(' ');
+					if (r < 10)
+					{
+						_putchar(' ');
+					}
+				}
+				if (r < 10)
+				{
+					_putchar(r + '0');
+				} else
+				{
+					print_digit(r);
+				}
+				if (b != 9)
+				{
+					_putchar(',');
+				}
+				b++;
+			}
+			_putchar('\n');
+			a++;
+		}
+	}
+}
