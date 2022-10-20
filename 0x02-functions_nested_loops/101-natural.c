f5ab3e9e13e95e27612e2a6158ea6351c74d164f
Namkere Akpabio <donakpabio@gmail.com>
2022-10-19 20:18:39 -0700

    In computer class...

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * naturual_num - check the code.
+ *
+ * Return: Always Nothing
+ */
+void naturual_num()
+{
+
+	int s3 = 0;
+	int s5 = 0;
+	int a = 3;
+
+	while (a < 1024)
+	{
+		s3 = s3 + a;
+		a = a + 3;
+	}
+
+	a = 5;
+
+	while (a < 1024)
+	{
+		s5 = s5 + a;
+		a = a + 5;
+	}
+
+	printf("The answer is: %d\n", (s3 + s5));
+}
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	naturual_num();
+	return (0);
+}
