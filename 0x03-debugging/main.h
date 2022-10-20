3c87513a2367e1deaf7f9cffaa812a84b42f2a3c
Namkere Akpabio <donakpabio@gmail.com>
2022-10-20 09:41:54 -0700

    Test

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,7 +5,7 @@
 
 /**
  * positive_or_negative - writes the character c to stdout
- * @i: The character to print
+ *@i: The character to print
  *
  * Return: Nothing
  */
@@ -13,9 +13,9 @@ void positive_or_negative(int i);
 
 /**
  * largest_number - returns the largest of 3 numbers
- * @a: first integer
- * @b: second integer
- * @c: third integer
+ *@a: first integer
+ *@b: second integer
+ *@c: third integer
  * Return: largest number
  */
 int largest_number(int a, int b, int c);
@@ -23,9 +23,9 @@ int largest_number(int a, int b, int c);
 /**
 * print_remaining_days - takes a date and prints how many days are
 * left in the year, taking leap years into account
-* @month: month in number format
-* @day: day of month
-* @year: year
+*@month: month in number format
+*@day: day of month
+*@year: year
 * Return: void
 */
 void print_remaining_days(int month, int day, int year);
@@ -33,8 +33,8 @@ void print_remaining_days(int month, int day, int year);
 /**
 * convert_day - converts day of month to day of year, without accounting
 * for leap year
-* @month: month in number format
-* @day: day of month
+*@month: month in number format
+*@day: day of month
 * Return: day of year
 */
 int convert_day(int month, int day);
