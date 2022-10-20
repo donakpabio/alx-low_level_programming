3dc4c926531872db9f569e48b2df123cb8bf418d
Namkere Akpabio <donakpabio@gmail.com>
2022-10-20 01:42:25 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _isupper - check the code.
+ *
+ * Return: result
+ */
+void _isupper(int c)
+{
+	return (isupper(c) > 0 ? 1 : 0);	
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,109 @@
+#include <stdio.h>
+
+/**
+ * _putchar - check the code.
+ *
+ * Return: Always Nothing.
+ */
+void _putchar(int c);
+
+/**
+ * print_alphabet - check the code.
+ *
+ * Return: Always Nothing.
+ */
+void print_alphabet();
+
+/**
+ * print_alphabet_x10 - check the code.
+ *
+ * Return: Always Nothing
+ */
+void print_alphabet_x10();
+
+/**
+ * _islower - check the code.
+ *@c: something here
+ *
+ * Return: Always one or zero
+ */
+int _islower(int c);
+
+/**
+ * _isupper - check the code.
+ *@c: something here
+ *
+ * Return: Always one or zero
+ */
+int _isupper(int c);
+
+/**
+ * _isalpha - check the code.
+ *@c: something here
+ *
+ * Return: Always one or zero
+ */
+int _isalpha(int c);
+
+/**
+ * print_sign - check the code.
+ *@n: something here
+ *
+ * Return: Always one or zero
+ */
+int print_sign(int n);
+
+/**
+ * _abs - check the code.
+ *@n: something here
+ *
+ * Return: result
+ */
+int _abs(int n);
+
+/**
+ * print_last_digit - check the code.
+ *@n: something here
+ *
+ * Return: result
+ */
+int print_last_digit(int n);
+
+/**
+ * jack_bauer - check the code.
+ *
+ * Return: result
+ */
+int jack_bauer(void);
+
+/**
+ * times_table - check the code.
+ *
+ * Return: Nothing
+ */
+void times_table(void);
+
+/**
+ * add - writes the character c to stdout
+ * @a: The character to print
+ * @b: The character to print
+ *
+ * Return: result
+ */
+int add(int a, int b);
+
+/**
+ * print_to_98 - writes the character c to stdout
+ * @n: The character to print
+ *
+ * Return: Nothing
+ */
+void print_to_98(int n);
+
+/**
+ * print_times_table - check the code.
+ * @n: the number
+ *
+ * Return: Nothing
+ */
+void print_times_table(int n);
