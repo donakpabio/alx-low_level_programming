2ecb0c8f588377c9a702f6ef64b0b45078e13fdc
Namkere Akpabio <donakpabio@gmail.com>
2022-10-24 06:32:43 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Just reset
+ * @n: input
+ * Return - Nothing
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - Just reset
+ * @n: input
+ * Return - Nothing
+void reset_to_98(int *n);
