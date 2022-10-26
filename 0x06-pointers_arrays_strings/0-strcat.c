b3e512756dd7fbe5eb8dd0e7295b34bead479d59
Namkere Akpabio <donakpabio@gmail.com>
2022-10-26 07:32:23 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcat - Concat
+ * @dest: input
+ * @src: input
+ * Return: result
+ */
+char *_strcat(char *dest, char *src)
+{
+	return strcat(dest, src);
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * _strcat - Concat
+ * @dest: input
+ * @src: input
+ * Return: result
+ */
+char *_strcat(char *dest, char *src);
+ 
