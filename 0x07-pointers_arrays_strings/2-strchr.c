6ba06d64bbc5448c0350d9ed076a53c56fd319fd
Namkere Akpabio <donakpabio@gmail.com>
2022-10-31 14:29:22 -0700

    Tse

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strchr - Do something
+ * @s: input
+ * @c: input
+ * Return: result
+ */
+char *_strchr(char *s, char c)
+{
+	return (strchr(s, c));
+}
