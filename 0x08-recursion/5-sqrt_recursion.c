e3e67a1243fafbb63664ca84b8475367ebb0b71e
Namkere Akpabio <donakpabio@gmail.com>
2022-11-02 04:35:09 -0700

    Your subconscious is looking...

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - Do something
+ * @n: input
+ * Return: result
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (sqrt(n));
+}
