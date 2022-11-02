f40996bf28c59330ed2f06727b3ea7edaf9bee42
Namkere Akpabio <donakpabio@gmail.com>
2022-11-02 02:20:52 -0700

    Why is it so important...

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,19 +2,28 @@
 #include "main.h"
 
 /**
+ * _print_reverse - Do something
+ * @s: input
+ * @p: input
+ * Return: Nothing
+ */
+void _print_reverse(char *s, int p)
+{
+	_putchar(*(s + p - 1));
+	p--;
+	if (p > 0)
+	{
+		_print_reverse(s, p);
+	}
+}
+
+/**
  * _print_rev_recursion - Do something
  * @s: input
  * Return: Nothing
  */
 void _print_rev_recursion(char *s)
 {
-	int l = strlen(s);
-	int i = 0;
+	_print_reverse(s, strlen(s));
 
-	while (i < l)
-	{
-		_putchar(*(s + l - i - 1));
-		i++;
-	}
-	_putchar('\n');
 }
