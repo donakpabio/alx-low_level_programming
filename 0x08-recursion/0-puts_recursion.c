0f8b9aa130a6263d286da8b292a7f6f9208ba108
Namkere Akpabio <donakpabio@gmail.com>
2022-11-02 01:41:46 -0700

    She locked away ...

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Do something
+ * @s: input
+ * Return: Nothing
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+/**
+ * _puts_recursion - Do something
+ * @s: input
+ * Return: Nothing
+ */
+void _puts_recursion(char *s);
+
+/**
+ * _print_rev_recursion - Do something
+ * @s: input
+ * Return: Nothing
+ */
+void _print_rev_recursion(char *s);
+
+/**
+ * _strlen_recursion - Do something
+ * @s: input
+ * Return: result
+ */
+int _strlen_recursion(char *s);
+
+/**
+ * factorial - Do something
+ * @n: input
+ * Return: result
+ */
+int factorial(int n);
+
+/**
+ * _pow_recursion - Do something
+ * @x: input
+ * @y: input
+ * Return: result
+ */
+int _pow_recursion(int x, int y);
+
+/**
+ * _sqrt_recursion - Do something
+ * @n: input
+ * Return: result
+ */
+int _sqrt_recursion(int n);
+
+/**
+ * is_prime_number - Do something
+ * @n: input
+ * Return: result
+ */
+int is_prime_number(int n);
+
+/**
+ * _putchar - Do something
+ * @c: input
+ * Return: result
+ */
+void _putchar(int c);
