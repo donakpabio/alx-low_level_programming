4d8c3ae087797c5b5f198772d94b8375ce465158
Namkere Akpabio <donakpabio@gmail.com>
2022-11-02 03:13:59 -0700

    Once an idea has taken hold ...

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,13 @@
+#include <math.h>
+#include "main.h"
+
+/**
+ * _pow_recursion - Do something
+ * @x: input
+ * @y: input
+ * Return: result
+ */
+int _pow_recursion(int x, int y)
+{
+	return (pow(x,y));
+}
