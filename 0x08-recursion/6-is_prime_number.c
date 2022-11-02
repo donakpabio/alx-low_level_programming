c5fa6cd25f53e25ef48f0099a89c172c8272ca0a
Namkere Akpabio <donakpabio@gmail.com>
2022-11-02 15:37:01 -0700

    Inception. Is it possible?

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,7 +8,7 @@
  */
 int is_prime(int n, int i)
 {
-	if ((n <= 1) || ((n % i == 0) && (n != i))
+	if ((n <= 1) || ((n % i == 0) && (n != i)))
 	{
 		return (0);
 	} else if (n == i)
@@ -16,8 +16,8 @@ int is_prime(int n, int i)
 		return (1);
 	}
 	i++;
-        return (is_prime(n, i));
-} 
+	return (is_prime(n, i));
+}
 
 /**
  * is_prime_number - Do something
@@ -27,27 +27,4 @@ int is_prime(int n, int i)
 int is_prime_number(int n)
 {
 	return (is_prime(n, 2));
-}
-
-int main(void)
-{
-    int r;
-
-    r = is_prime_number(1);
-    printf("%d\n", r);
-    r = is_prime_number(1024);
-    printf("%d\n", r);
-    r = is_prime_number(16);
-    printf("%d\n", r);
-    r = is_prime_number(17);
-    printf("%d\n", r);
-    r = is_prime_number(25);
-    printf("%d\n", r);
-    r = is_prime_number(-1);
-    printf("%d\n", r);
-    r = is_prime_number(113);
-    printf("%d\n", r);
-    r = is_prime_number(7919);
-    printf("%d\n", r);
-    return (0);
 }
