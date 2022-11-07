21dcae52f44b1aac7eb4a295be00d2adc5870592
Namkere Akpabio <donakpabio@gmail.com>
2022-11-07 08:01:53 -0800

    No zero

---
+++ 0x09-static_libraries/libmy.c
@@ -0,0 +1,216 @@
+#include "main.h"
+
+/**
+ * _putchar - Do something
+ * @c: input
+ * Return: result
+ */
+int _putchar(char c)
+{
+	return (0);
+}
+
+/**
+ * _islower - Do something
+ * @c: input
+ * Return: result
+ */
+int _islower(int c)
+{
+	return (0);
+}
+
+/**
+ * _isalpha - Do something
+ * @c: input
+ * Return: result
+ */
+int _isalpha(int c)
+{
+	return (0);
+}
+
+/**
+ * _abs - Do something
+ * @n: input
+ * Return: result
+ */
+int _abs(int n)
+{
+	return (0);
+}
+
+/**
+ * _isupper - Do something
+ * @c: input
+ * Return: result
+ */
+int _isupper(int c)
+{
+	return (0);
+}
+
+/**
+ * _isdigit - Do something
+ * @c: input
+ * Return: result
+ */
+int _isdigit(int c)
+{
+	return (0);
+}
+
+/**
+ * _strlen - Do something
+ * @s: input
+ * Return: result
+ */
+int _strlen(char *s)
+{
+	return (0);
+}
+
+/**
+ * _puts - Do something
+ * @s: input
+ * Return: Nothing
+ */
+void _puts(char *s)
+{
+	return;
+}
+
+/**
+ * _strcpy - Do something
+ * @dest: input
+ * @src: input
+ * Return: result
+ */
+char *_strcpy(char *dest, char *src)
+{
+	return NULL;
+}
+
+/**
+ * atoi - Do something
+ * @s: input
+ * Return: result
+ */
+int _atoi(char *s)
+{
+	return (0);
+}
+
+/**
+ * _strcat - Do something
+ * @dest: input
+ * @src: input
+ * Return: result
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (0);
+}
+
+/**
+ * _strncat - Do something
+ * @dest: input
+ * @src: input
+ * @n: input
+ * Return: result
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	return (0);
+}
+
+/**
+ * _strncpy - Do something
+ * @dest: input
+ * @src: input
+ * @n: input
+ * Return: result
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	return (0);
+}
+
+/**
+ * _strcmp - Do something
+ * @s1: input
+ * @s2: input
+ * Return: result
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (0);
+}
+
+/**
+ * _memset - Do something
+ * @s: input
+ * @b: input
+ * @n: input
+ * Return: result
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	return (0);
+}
+
+/**
+ * _memcpy - Do something
+ * @dest: input
+ * @src: input
+ * @n: input
+ * Return: result
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	return (0);
+}
+
+/**
+ * _strchr - Do something
+ * @s: input
+ * @c: input
+ * Return: result
+ */
+char *_strchr(char *s, char c)
+{
+	return (0);
+}
+
+/**
+ * _strspn - Do something
+ * @s: input
+ * @accept: input
+ * Return: result
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	return (0);
+}
+
+/**
+ * _strpbrk - Do something
+ * @s: input
+ * @accept: input
+ * Return: result
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	return (0);
+}
+
+/**
+ * _strstr - Do somehting
+ * @haystack: input
+ * @needle: input
+ * Return: result
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	return (0);
+}
---
+++ 0x09-static_libraries/main.h
@@ -0,0 +1,155 @@
+#include <stdio.h>
+/**
+ * _putchar - Do something
+ * @c: input
+ * Return: result
+ */
+int _putchar(char c);
+
+/**
+ * _islower - Do something
+ * @c: input
+ * Return: result
+ */
+int _islower(int c);
+
+/**
+ * _isalpha - Do something
+ * @c: input
+ * Return: result
+ */
+int _isalpha(int c);
+
+/**
+ * _abs - Do something
+ * @n: input
+ * Return: result
+ */
+int _abs(int n);
+
+/**
+ * _isupper - Do something
+ * @c: input
+ * Return: result
+ */
+int _isupper(int c);
+
+/**
+ * _isdigit - Do something
+ * @c: input
+ * Return: result
+ */
+int _isdigit(int c);
+
+/**
+ * _strlen - Do something
+ * @s: input
+ * Return: result
+ */
+int _strlen(char *s);
+
+/**
+ * _puts - Do something
+ * @s: input
+ * Return: Nothing
+ */
+void _puts(char *s);
+
+/**
+ * _strcpy - Do something
+ * @dest: input
+ * @src: input
+ * Return: result
+ */
+char *_strcpy(char *dest, char *src);
+
+/**
+ * atoi - Do something
+ * @s: input
+ * Return: result
+ */
+int _atoi(char *s);
+
+/**
+ * _strcat - Do something
+ * @dest: input
+ * @src: input
+ * Return: result
+ */
+char *_strcat(char *dest, char *src);
+
+/**
+ * _strncat - Do something
+ * @dest: input
+ * @src: input
+ * @n: input
+ * Return: result
+ */
+char *_strncat(char *dest, char *src, int n);
+
+/**
+ * _strncpy - Do something
+ * @dest: input
+ * @src: input
+ * @n: input
+ * Return: result
+ */
+char *_strncpy(char *dest, char *src, int n);
+
+/**
+ * _strcmp - Do something
+ * @s1: input
+ * @s2: input
+ * Return: result
+ */
+int _strcmp(char *s1, char *s2);
+
+/**
+ * _memset - Do something
+ * @s: input
+ * @b: input
+ * @n: input
+ * Return: result
+ */
+char *_memset(char *s, char b, unsigned int n);
+
+/**
+ * _memcpy - Do something
+ * @dest: input
+ * @src: input
+ * @n: input
+ * Return: result
+ */
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+/**
+ * _strchr - Do something
+ * @s: input
+ * @c: input
+ * Return: result
+ */
+char *_strchr(char *s, char c);
+
+/**
+ * _strspn - Do something
+ * @s: input
+ * @accept: input
+ * Return: result
+ */
+unsigned int _strspn(char *s, char *accept);
+
+/**
+ * _strpbrk - Do something
+ * @s: input
+ * @accept: input
+ * Return: result
+ */
+char *_strpbrk(char *s, char *accept);
+
+/**
+ * _strstr - Do somehting
+ * @haystack: input
+ * @needle: input
+ * Return: result
+ */
+char *_strstr(char *haystack, char *needle);
