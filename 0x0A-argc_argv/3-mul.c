f6a94b427b8cb3cb4cbc3db09dfa889e622a9941
Namkere Akpabio <donakpabio@gmail.com>
2022-11-08 13:18:22 -0800

    Test

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,15 +8,17 @@
  */
 int main(int argc, char **argv)
 {
+	int a = 0, b = 0, c = 0;
+
 	if ((argc - 1) != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	int a = atoi(*(argv + 1));
-	int b = atoi(*(argv + 2));
+	a = atoi(*(argv + 1));
+	b = atoi(*(argv + 2));
 
-	int c = a * b;
+	c = a * b;
 
 	printf("%d\n", c);
 	exit(EXIT_SUCCESS);
