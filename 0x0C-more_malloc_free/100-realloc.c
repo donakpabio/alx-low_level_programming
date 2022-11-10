11b1867d406010827fc37b092f57a8012d12cf6c
Namkere Akpabio <donakpabio@gmail.com>
2022-11-10 05:49:53 -0800

    test

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -9,5 +9,14 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
 	return (realloc(ptr, old_size, new_size));
 }
