497d61976556e6c1c2372513776932dbdce70e0e
Namkere Akpabio <donakpabio@gmail.com>
2022-11-11 00:36:56 -0800

    test

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,14 +10,14 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int l1 = 0, l2 = 0, t = 0; 
-	int i = 0, j = 0;
+	int m = (int) n;
+	int l1 = 0, l2 = 0, t = 0, i = 0, j = 0;
 	char *r;
 
 	l1 = s1 == NULL ? 0 : strlen(s1);
 	l2 = s2 == NULL ? 0 : strlen(s2);
 
-	l2 = n >= l2 ? l2 : n >= 0 ? n : 0;
+	l2 = m >= l2 ? l2 : m >= 0 ? m : 0;
 	t = l1 + l2;
 
 	r = malloc(t + 1);
