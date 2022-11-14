8e6c59dace9d22d2b4787f2db1fd8e52e931fb44
Namkere Akpabio <donakpabio@gmail.com>
2022-11-14 15:59:30 -0800

    test

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+/**
+ * init_dog - Do something
+ * @d: input
+ * @name: input
+ * @age: input
+ * @owner: input
+ * Return: Nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d.name = name;
+	d.age = age;
+	d.owner = owner;
+}
