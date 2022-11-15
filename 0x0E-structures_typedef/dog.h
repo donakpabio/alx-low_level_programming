68ed21aa0dfe363df6084fc26cab4de3baf48f5c
Namkere Akpabio <donakpabio@gmail.com>
2022-11-14 16:27:29 -0800

    test

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,3 +15,10 @@ typedef struct dog dog_t;
  * Return: result
  */
 dog_t *new_dog(char *name, float age, char *owner);
+
+/**
+ * print_dog - Do something
+ * @d: input
+ * Return: Nothing
+ */
+void print_dog(struct dog *d);
