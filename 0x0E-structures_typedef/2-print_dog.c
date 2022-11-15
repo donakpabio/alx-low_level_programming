83877199375dd94af39e152ed7ff9a485c09671a
Namkere Akpabio <donakpabio@gmail.com>
2022-11-15 01:52:45 -0800

    tes

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,7 +11,7 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		printf("Name: %s\n", (*d).name == NULL ? "(nil)" : (*d).name);
-		printf("Age %1f\n", (*d).age);
-		printf("Owner %s\n", (*d).owner == NULL ? "(nil)" : (*d).owner);
+		printf("Age: %1f\n", (*d).age);
+		printf("Owner: %s\n", (*d).owner == NULL ? "(nil)" : (*d).owner);
 	}
 }
