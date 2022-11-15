dc657e681ad2050790cc8b3cd1c3154c89b4f27f
Namkere Akpabio <donakpabio@gmail.com>
2022-11-15 11:55:01 -0800

    h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,3 @@
-#ifndef DOG
-#define DOG
 /**
  * struct dog - Soemthing about it
  * @name: input
@@ -13,49 +11,3 @@ struct dog
 	float age;
 	char *owner;
 };
-#endif
-
-#ifndef DOG_T
-#define DOG_T
-/**
- * dog_t - typedef for dog
- * Description: Something about it
- */
-typedef struct dog dog_t;
-#endif
-
-/**
- * new_dog - Do somthing
- * @name: input
- * @age: input
- * @owner: input
- * Return: result
- */
-dog_t *new_dog(char *name, float age, char *owner);
-
-
-/**
- * print_dog - Do something
- * @d: input
- * Return: Nothing
- */
-void print_dog(struct dog *d);
-
-
-/**
- * init_dog - Do something
- * @d: input
- * @name: input
- * @age: input
- * @owner: input
- * Return: Nothing
- */
-void init_dog(struct dog *d, char *name, float age, char *owner);
-
-
-/**
- * free_dog - Do something
- * @d: input
- * Return: Nothing
- */
-void free_dog(dog_t *d);
