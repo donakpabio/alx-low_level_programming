a5c4cd8efce397ac9f449729f8db9b6ce6a9f880
Namkere Akpabio <donakpabio@gmail.com>
2022-11-15 09:59:21 -0800

    t

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "dog.h"
 
 /**
@@ -10,10 +11,19 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if(d != NULL)
+	if (d != NULL)
 	{
 		(*d).name = name;
 		(*d).age = age;
 		(*d).owner = owner;
 	}
+}
+
+int main(void)
+{
+    struct dog my_dog;
+
+    init_dog(NULL, "Poppy", 3.5, "Bob");
+    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+    return (0);
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,9 @@
-#ifndef dog
 /**
- * dog - struct
+ * struct dog - Soemthing about it
+ * @name: input
+ * @age: input
+ * @owner: input
+ * Description: Just dog
  */
 struct dog
 {
@@ -8,12 +11,15 @@ struct dog
 	float age;
 	char *owner;
 };
-#endif
-#ifndef dog_t
-typedef struct dog dog_t;
-#endif
 
-#ifndef new_dog
+
+/**
+ * dog_t - typedef for dog
+ * Description: Something about it
+ */
+typedef struct dog dog_t;
+
+
 /**
  * new_dog - Do somthing
  * @name: input
@@ -22,18 +28,16 @@ typedef struct dog dog_t;
  * Return: result
  */
 dog_t *new_dog(char *name, float age, char *owner);
-#endif
 
-#ifndef print_dog
+
 /**
  * print_dog - Do something
  * @d: input
  * Return: Nothing
  */
 void print_dog(struct dog *d);
-#endif
 
-#ifndef init_dog
+
 /**
  * init_dog - Do something
  * @d: input
@@ -43,13 +47,11 @@ void print_dog(struct dog *d);
  * Return: Nothing
  */
 void init_dog(struct dog *d, char *name, float age, char *owner);
-#endif
 
-#ifndef free_dog
+
 /**
  * free_dog - Do something
  * @d: input
  * Return: Nothing
  */
 void free_dog(dog_t *d);
-#endif
