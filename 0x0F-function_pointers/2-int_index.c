8ea94f9240473e4657f92951849b5714b56ad1c9
Namkere Akpabio <donakpabio@gmail.com>
2022-11-16 13:49:10 -0800

    d

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,8 +9,20 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
+	int i;
+
+	i = 0;
 	if(size <= 0)
 	{
 		return (-1);
-	}	
+	}
+
+	while (i < size)
+	{
+		if((*cmp)(*(array + i)))
+		{
+			return (i);
+		}
+	}
+	return (-1);
 }
