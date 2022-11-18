c6e9234828dce7ab0d5488abc2e94423010aa411
Namkere Akpabio <donakpabio@gmail.com>
2022-11-17 19:36:22 -0800

    m

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -24,21 +24,8 @@ int sum_them_all(const unsigned int n, ...)
 	while (i > 0)
 	{
 		t = t + va_arg(ap, int);
-		printf("(%d, %d) ", i, t);
 		i--;
 	}
-	putchar('\n');
 	va_end(ap);
 	return (t);
-}
-
-int main(void)
-{
-    int sum;
-
-    sum = sum_them_all(2, 98, 1024);
-    printf("%d\n", sum);
-    sum = sum_them_all(4, 98, 1024, 402, -1024);
-    printf("%d\n", sum);
-    return (0);
 }
