1232b147303cd310b155148580313920f4104ceb
Namkere Akpabio <donakpabio@gmail.com>
2022-11-24 13:34:38 -0800

    m

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - Do something
+ * @head: input
+ * Return: result
+ */
+void free_list(list_t *head)
+{
+	if (head->next != NULL)
+	{
+		free_list(head->next);
+	}
+	free(head->str);
+	free(head);
+}
