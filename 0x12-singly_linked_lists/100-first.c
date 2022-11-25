21b00f525dc59cdc86df881123f1975ffd7fecd1
Namkere Akpabio <donakpabio@gmail.com>
2022-11-25 05:31:11 -0800

    f

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,7 +1,15 @@
-#include <stdio.c>
+#include <stdio.h>
 
+/**
+ * myStartupFun - Do something
+ * Return: Nothing
+ */
 void myStartupFun(void) __attribute__((constructor));
 
+/**
+ * myStartupFun - Do something
+ * Return: Nothing
+ */
 void myStartupFun(void)
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
