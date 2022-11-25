2f4a423fc0ff2e2e6662f1c2d76d86c847e2cc0a
Namkere Akpabio <donakpabio@gmail.com>
2022-11-25 05:07:06 -0800

    t

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -9,30 +9,12 @@
  */
 void free_list(list_t *head)
 {
-	if (head == NULL)
+	if (head != NULL)
 	{
-		exit(EXIT_FAILURE);
+		if (head->next != NULL)
+		{
+			free_list(head->next);
+		}
+		free(head);
 	}
-
-	if (head->next != NULL)
-	{
-		free_list(head->next);
-	}
-	free(head);
-}
-
-int main(void)
-{
-    list_t *head;
-
-    head = NULL;
-    /*add_node_end(&head, "Bob");
-    add_node_end(&head, "&");
-    add_node_end(&head, "Kris");
-    add_node_end(&head, "love");*/
-    add_node_end(&head, "asm");
-    print_list(head);
-    free_list(head);
-    head = NULL;
-    return (0);
 }
