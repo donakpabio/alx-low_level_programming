2863f392d64c7e5716a0ee5eae1e7efb8dcfc0b1
Namkere Akpabio <donakpabio@gmail.com>
2022-11-25 01:57:31 -0800

    m

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,7 +13,7 @@ size_t print_list(const list_t *h)
 
 	if (h == NULL)
 	{
-		return (0);
+		exit(EXIT_FAILURE);
 	}
 
 	isnull = h->str == NULL;
--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -11,7 +11,7 @@ size_t list_len(const list_t *h)
 {
 	if (h == NULL)
 	{
-		return (0);
+		exit(EXIT_FAILURE);
 	}
 
 	if (h->next != NULL)
