b62a00a996f6ce3a81b57c4f0d87e55a12dedac4
Namkere Akpabio <donakpabio@gmail.com>
2022-11-25 05:00:09 -0800

    i

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,12 +11,13 @@ size_t print_list(const list_t *h)
 {
 	int isnull;
 
-	isnull = (h == NULL) || (h->str == NULL);
-	printf("[%u] %s\n", isnull ? 0 : h->len, isnull ? "(nil)" : h->str);
 	if (h == NULL)
 	{
-		exit(EXIT_FAILURE);
+		return (0);
 	}
+
+	isnull = (h == NULL) || (h->str == NULL);
+	printf("[%u] %s\n", isnull ? 0 : h->len, isnull ? "(nil)" : h->str);
 
 	if (h->next != NULL)
 	{
