acc5d0da8a22f602149d9d04f23020206924ddc8
Namkere Akpabio <donakpabio@gmail.com>
2022-11-28 13:27:22 -0800

    i

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,12 +20,12 @@ listint_t *node_at(listint_t *head, int c, int at)
 	{
 		return (head);
 	}
-	
+
 	if (head->next == NULL)
 	{
 		return (NULL);
 	}
-	return (node_at(head->next, c++, at));
+	return (node_at(head->next, ++c, at));
 }
 
 /**
@@ -47,36 +47,15 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		return (NULL);
 	}
-	
+
 	new->n = n;
 	at = node_at(*head, 0, index - 1);
 	if (at == NULL)
 	{
 		return (NULL);
 	}
-	
+
 	new->next = at->next;
 	at->next = new;
 	return (new);
-}
-
-int main(void)
-{
-    listint_t *head;
-
-    head = NULL;
-    add_nodeint_end(&head, 0);
-    add_nodeint_end(&head, 1);
-    add_nodeint_end(&head, 2);
-    add_nodeint_end(&head, 3);
-    add_nodeint_end(&head, 4);
-    add_nodeint_end(&head, 98);
-    add_nodeint_end(&head, 402);
-    add_nodeint_end(&head, 1024);
-    print_listint(head);
-    printf("-----------------\n");
-    insert_nodeint_at_index(&head, 5, 4096);
-    print_listint(head);
-    free_listint2(&head);
-    return (0);
 }
