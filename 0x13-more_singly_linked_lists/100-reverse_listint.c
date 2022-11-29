1bdb2ae20f8476059a74f27448e9a1ef2ff3b7fc
Namkere Akpabio <donakpabio@gmail.com>
2022-11-29 08:36:29 -0800

    w

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -2,6 +2,62 @@
 #include "lists.h"
 
 /**
+ * node_at - Do something
+ * @head: input
+ * @c: counter
+ * @at: at
+ * Return: result
+ */
+listint_t *node_at(listint_t *head, int c, int at)
+{
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+
+	if (c == at)
+	{
+		return (head);
+	}
+
+	if (head->next == NULL)
+	{
+		return (NULL);
+	}
+	return (node_at(head->next, ++c, at));
+}
+
+/**
+ * get_nodeint_at_index - Do something
+ * @head: input
+ * @index: input
+ * Return: result
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	return (node_at(head, 0, (int) index));
+}
+
+/**
+ * listint_len - Do something
+ * @h: input
+ * Return: result
+ */
+size_t listint_len(const listint_t *h)
+{
+	if (h == NULL)
+	{
+		return (0);
+	}
+
+	if (h->next != NULL)
+	{
+		return (listint_len(h->next) + 1);
+	}
+	return (1);
+}
+
+/**
  * reverse_listint - Do something
  * @head: input
  * Return: result
