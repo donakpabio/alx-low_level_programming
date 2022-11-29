6d0a62f274914cdf5077b3deeadf14d567763153
Namkere Akpabio <donakpabio@gmail.com>
2022-11-29 08:49:59 -0800

    w

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -66,12 +66,15 @@ listint_t *reverse_listint(listint_t **head)
 {
 	int d;
 	listint_t *t;
+	int r;
 
+	r = 0;
 	d = ((int) listint_len(*head)) - 1;
 	t = get_nodeint_at_index(*head, d);
 	while (d >= 0)
 	{
-		(get_nodeint_at_index(*head, d))->next = d -1 >= 0 ? get_nodeint_at_index(*head, d - 1) : NULL;
+		r = d - 1;
+		(get_nodeint_at_index(*head, d))->next = r >= 0 ? get_nodeint_at_index(*head, r) : NULL;
 		d--;
 	}
 	*head = t;
