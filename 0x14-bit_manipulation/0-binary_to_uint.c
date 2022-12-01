76c5399e954c5f149db44204fee3162d0994890c
Namkere Akpabio <donakpabio@gmail.com>
2022-12-01 11:57:27 -0800

    a

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,52 @@
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+#include "main.h"
+
+int power(int x, int y)
+{
+	int i, n;
+
+	i = 0;
+	n = 1;
+
+	while (i < y)
+	{
+		n = n * x;
+		i++;
+	}
+	return (n);
+}
+
+/**
+ * binary_to_uint - Do something
+ * @b: input
+ * Return: result
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int l, n, y, x, z;
+	if (b == NULL)
+	{
+		return (0);
+	}
+
+	l = strlen(b) - 1;
+	n = 0;
+	y = 0;
+	z = 0;
+	x = 2;
+
+	while (l >= 0)
+	{
+		if (!isdigit(*(b + l)))
+		{
+			return (0);
+		}
+		z = *(b + l) - '0';
+		n = n + (power(x, y) * z);
+		l--;
+		y++;
+	}
+	return n;
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,14 @@
+#ifndef STDIO_H
+#define STDIO_H
+#include <stdio.h>
+#endif
+
+#ifndef BINARY_TO_UINT_H
+#define BINARY_TO_UINT_H
+/**
+ * binary_to_uint - Do something
+ * @b: input
+ * Return: result
+ */
+unsigned int binary_to_uint(const char *b);
+#endif
