5528f770e4fe83eb3d50750c8a63784343c356bd
Namkere Akpabio <donakpabio@gmail.com>
2022-12-06 09:18:36 -0800

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,12 +12,12 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	int size;
 
-	if(filename == NULL)
+	if (filename == NULL)
 	{
 		return (-1);
 	}
 
-	fd = open(filename, O_RDWR |O_CREAT | O_EXCL | O_TRUNC);
+	fd = open(filename, O_RDWR | O_CREAT | O_EXCL | O_TRUNC);
 	if (fd == -1)
 	{
 		return (-1);
