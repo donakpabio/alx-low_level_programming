2982077ed71d49286c084ea70f4367d11f4415ef
Namkere Akpabio <donakpabio@gmail.com>
2022-12-06 09:19:39 -0800

    a

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -20,7 +20,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 
 	text = calloc(letters, sizeof(char));
+	if (text == NULL)
+	{
+		return (0);
+	}
+
 	fd = open(filename, O_RDONLY);
+
 	if (fd == -1)
 	{
 		return (0);
