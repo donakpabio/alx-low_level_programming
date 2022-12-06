dac574ffc0c99bcf6636d2a417e1fac5fdc18425
Namkere Akpabio <donakpabio@gmail.com>
2022-12-06 09:35:10 -0800

    a

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,36 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * append_text_to_file - Do something
+ * @filename: input
+ * @text_content: input
+ * Return: result
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	fd = open(filename, O_WRONLY | O_EXCL | O_APPEND);
+	if (fd = -1)
+	{
+		return (-1);
+	}
+
+	if (text_content == NULL)
+	{
+		return (-1);
+	}
+	if (write(fd, text_content, strlen(text_content)) <= 0)
+	{
+		return (-1);
+	}
+
+	close(fd);
+	return (1);	
+}
