573bc5282371db3901b44aaf00189b0f1079c5b2
Namkere Akpabio <donakpabio@gmail.com>
2022-12-09 12:41:00 -0800

    h

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <unistd.h>
 #include "main.h"
 
 /**
@@ -16,9 +17,10 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	fd = open(filename, O_RDWR | O_EXCL);
-	if (fd = -1)
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
 	{
+		printf("Issue is here");
 		return (-1);
 	}
 
