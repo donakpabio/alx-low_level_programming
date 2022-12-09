9356894624cda9275983bc87594f7ff167abc73e
Namkere Akpabio <donakpabio@gmail.com>
2022-12-09 12:48:05 -0800

    h

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -24,15 +24,13 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	if (text_content == NULL)
+	if (text_content != NULL)
 	{
-		return (-1);
+		if (write(fd, text_content, strlen(text_content)) <= 0)
+		{
+			return (-1);
+		}
 	}
-	if (write(fd, text_content, strlen(text_content)) <= 0)
-	{
-		return (-1);
-	}
-
 	close(fd);
 	return (1);
 }
