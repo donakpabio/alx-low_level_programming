2f9b5ff4fc4a609867ea3b5ff436276cdd031b37
Namkere Akpabio <donakpabio@gmail.com>
2022-12-09 20:44:34 -0800

    p

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,6 +26,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		write(fd, text_content, strlen(text_content));
 	}
+	printf("FD is %d\n", fd);
 	close(fd);
 	return (1);
 }
--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,56 +9,71 @@
  */
 int main(int ac, char **av)
 {
-    int res;
-    char *buf;
-    char *file_to;
-    char *file_from;
+    	char *file_to;
+    	char *file_from;
+	int i, fd1, fd2, sum, total;
 
-    if (ac != 3)
-    {
-        printf("Usage: cp file_from file_to\n");
-        exit(97);
-    }
+	sum = 1024/sizeof(char);
+	total = 0;
+	i = 0;
 
-    file_from = av[1];
-    file_to = av[2];
+	if (ac != 3)
+	{
+		printf("Usage: cp file_from file_to\n");
+		exit(97);
+	}
 
-    if (file_from == NULL)
-    {
-	    printf("Error: Can't read from file $s\n", file_from);
-	    exit(98);
-    }
-    fd1 = open(file_from, O_RDONLY);
+	file_from = av[1];
+	file_to = av[2];
 
-    if (fd1 == -1)
-    {
-	    printf("Error: Can't read from file %s\n", file_from);
-	    exit(98);
-    }
-    fd2 = open(file_to, O_WRONLY | O_TRUNC);
-    if (fd2 == -1)
-    {
-	    printf("Error: Can't write to %s\n", file_to);
-	    return (99);
-    }
-    
-    if (close(fd1) == -1)
-    {
-	    printf("Error: Can't close fd %FD_VALUE\n", fd1);
-	    return (100);
-    }
+	if (file_from == NULL)
+	{
+		printf("Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+	fd1 = open(file_from, O_RDONLY);
 
-    if (close(fd2) == -1)
-    {
-	    prinf("Error: Can't close fd %FD_VALUE\n", fd2);
-    }
-    buf = malloc(1024 * sizeof(char));
-    if (buf == NULL)
-    {
-	    exit(98);
-    }
+	if (fd1 == -1)
+	{
+		printf("Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+	fd2 = open(file_to, O_WRONLY | O_TRUNC);
+	if (fd2 == -1)
+	{
+		fd2 = open(file_to, O_CREAT, 664);
+		if (fd2 == -1)
+		{
+			printf("Error: Can't write to %s\n", file_to);
+			exit(99);
+		}
+	}	
 
-    res = append_text_to_file(av[1], av[2]);
-    printf("-> %i)\n", res);
-    return (0);
+	total = lseek(fd1, 0, SEEK_END);
+
+	while (i <= (total/sum))
+	{
+		int sz;
+		char *b;
+
+		sz = ((i * sum) + sum) < total ? sum : total - (i * sum);
+		b = malloc(sz * sizeof(char));
+		printf("Size is %d\n", sz);	
+		read(fd1, b, sz);
+		printf("Data is %s with size %lu\n", b, sizeof(b));
+		write(fd2, b, sz);
+		free(b);
+		i++;
+	}
+	
+	if (close(fd1) == -1)
+	{
+		printf("Error: Can't close fd %d", fd1);
+		exit(100);
+	}
+	if (close(fd2) == -1)
+	{
+		printf("Error: Can't close fd %d", fd2);
+		exit(100);
+	}
 }
