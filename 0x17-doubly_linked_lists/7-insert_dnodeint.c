76a4443c972b00c3b74c7e4eac73450670bcb1e7
Namkere Akpabio <donakpabio@gmail.com>
2023-01-12 12:51:29 -0800

    t

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,76 @@
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * node_at - Do something
+ * @head: input
+ * @c: counter
+ * @at: at
+ * Return: result
+ */
+dlistint_t *node_at(dlistint_t *head, int c, int at)
+{
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+
+	if (c == at)
+	{
+		return (head);
+	}
+
+	if (head->next == NULL)
+	{
+		return (NULL);
+	}
+	return (node_at(head->next, ++c, at));
+}
+
+/**
+ * insert_dnodeint_at_index - Do something
+ * @head: input
+ * @idx: input
+ * @n: input
+ * Return: result
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int idx, int n)
+{
+	dlistint_t *new;
+	dlistint_t *at;
+	dlistint_t *d;
+	int index;
+
+	index = (int) idx;
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+
+	new->n = n;
+	at = node_at(*head, 0, index - 1);
+	if (at == NULL)
+	{
+		if (index != 0)
+		{
+			return (NULL);
+		}
+		d = *head;
+		new->next = d;
+		new->prev = d->prev;
+		d->prev = new;
+		*head = new;
+		return (new);
+	}
+
+	new->next = at->next;
+	new->prev = at;
+	at->next = new;
+	if (new->next != NULL)
+	{
+		new->next->prev = new;
+	}
+	return (new);
+}
